62b4a64648fb2937984b324f0d9b7d7261967013
egrazm <eliasgonram@gmail.com>
2025-10-07 14:41:39 -0300

    feat:agrega minus a la lista retornada

--- dato_palabras.py
+++ dato_palabras.py
@@ -3,3 +3,5 @@ import random
 def obtener_lista_palabras():
     lista=["barro","juego","zumba","calor","hielo","lanza","borde","salsa","pulso"]
     return lista
+
+def seleccionar_palabra_secreta():
