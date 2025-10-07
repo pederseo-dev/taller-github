1d479d830766e5fbcac35b14cbb11612aa56ece8
egrazm <eliasgonram@gmail.com>
2025-10-07 14:59:44 -0300

    refactor: mejora nombre de variables

--- dato_palabras.py
+++ dato_palabras.py
@@ -5,18 +5,18 @@ def obtener_lista_palabras():
     """
     Funcion que genera una lista de 20 palabras, asegura que esten en minuscula con .lower() y retorna la lista para utilizarla luego.
     """
-    lista = [
+    lista_de_palabras = [
     "barro", "juego", "zumba", "calor", "hielo", "lanza", "borde", "salsa", "pulso",
     "brisa", "tacho", "fluir", "nubes", "vigor", "joven", "raton",
     "marea", "llama", "tenaz", "crisp"
 ]
-    lista_minus=[palabra.lower() for palabra in lista]
-    return lista_minus
-
+    lista_palabras_en_minuscula = [palabra.lower() for palabra in lista_de_palabras]
+    return lista_palabras_en_minuscula 
+ 
 def seleccionar_palabra_secreta():
     """
     Funcion que selecciona una palabra de la lista de manera aleatoria.
     """
-    palabra= obtener_lista_palabras()
-    return random.choice(palabra)
+    palabra_secreta = obtener_lista_palabras()
+    return random.choice(palabra_secreta)
 
