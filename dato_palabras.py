32877227a5e3cf71afe316c34b268fe2c6ac8282
egrazm <eliasgonram@gmail.com>
2025-10-07 14:43:05 -0300

    fix:agrega 20 palabras

--- dato_palabras.py
+++ dato_palabras.py
@@ -1,7 +1,10 @@
 import random
 
 def obtener_lista_palabras():
-    lista=["barro","juego","zumba","calor","hielo","lanza","borde","salsa","pulso"]
-    return lista
+    lista = [
+    "barro", "crisp", "zumba", "calor", "hielo", "lanza", "borde", "salsa", "pulso",
+    "brisa", "tacho", "tenaz", "nubes", "vigor", "joven", "raton",
+    "marea", "llama", "tenaz", "crisp"
+]
+    return lista.lower()
 
-def seleccionar_palabra_secreta():
