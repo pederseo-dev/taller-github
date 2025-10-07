e4fdd3fcc2fa4bd7a93d874f4d1c37a6e4b11bf9
egrazm <eliasgonram@gmail.com>
2025-10-07 14:49:05 -0300

    fix lista sin palabras repetidas

--- dato_palabras.py
+++ dato_palabras.py
@@ -2,8 +2,8 @@ import random
 
 def obtener_lista_palabras():
     lista = [
-    "barro", "crisp", "zumba", "calor", "hielo", "lanza", "borde", "salsa", "pulso",
-    "brisa", "tacho", "tenaz", "nubes", "vigor", "joven", "raton",
+    "barro", "juego", "zumba", "calor", "hielo", "lanza", "borde", "salsa", "pulso",
+    "brisa", "tacho", "fluir", "nubes", "vigor", "joven", "raton",
     "marea", "llama", "tenaz", "crisp"
 ]
     lista_minus=[palabra.lower() for palabra in lista]
@@ -13,5 +13,3 @@ def seleccionar_palabra_secreta():
     palabra= obtener_lista_palabras()
     return random.choice(palabra)
 
-pal=seleccionar_palabra_secreta()
-print(pal)
