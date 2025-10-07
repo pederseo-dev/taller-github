43978d3e7d168d68772147eca2a03c90c40744c7
Nicolas <nicofleitaspy@gmail.com>
2025-10-07 16:22:25 -0300

    fix manejo_entrada

--- manejo_entrada.py
+++ manejo_entrada.py
@@ -1,13 +1,23 @@
-def obtener_intento_jugador(numero_intento, intentos_maximos):
-        cantidad_intentos = 0
-        while cantidad_intentos < intentos_maximos:
-        
-            print(f"Intento {cantidad_intentos + 1}/{intentos_maximos}")
-            palabra_valida = input("Ingresa una palabra: ").strip().lower()
-            if validar_intento(palabra_valida):
-                  return palabra_valida
-            else:
-                print("Error")
-            cantidad_intentos += 1
+from validacion import validar_intento
 
+# Funcion para retornar palabra valida o un mensaje de error.
+
+def obtener_intento_jugador(numero_intento, intentos_maximos):
+
+    cantidad_intentos = 0
+
+    while True:
+            
+            print(f"Intento {cantidad_intentos + 1}/{intentos_maximos}")
+            # Solicitamos al usuario que ingrese una palabra y convertimos a minusculas y sacamos los espacios
+            palabra_valida = input("Ingresa una palabra: ").strip().lower() 
+            
+            # Desempaquetamos el return de validar_intento()
+            bandera, mensaje = validar_intento(numero_intento,len(palabra_valida))
+
+            # Si bandera == True
+            if bandera:
+                return palabra_valida
+            else:
+                print(mensaje)
 
