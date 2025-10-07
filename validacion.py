f0cc42e56d31a891ac2c3dbff9b749991fdb8ccb
Maximiliano <maxiajojot@gmail.com>
2025-10-07 14:51:50 -0300

    feat: verificar victoria

--- validacion.py
+++ validacion.py
@@ -7,5 +7,14 @@ def validar_intento(intento,longitud_palabra):
     else:
         return (False, "Solo se permiten Letras")
     
-
+def verificar_victoria(estados):
+    cantida_estados = len(estados)
+    contador = 0
+    for i in estados:
+        if i == "correcta":
+            contador += 1
+    if contador == cantida_estados:
+        return True
+    else:
+        return False
     
