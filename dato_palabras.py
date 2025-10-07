0cf7f943e620fd414fb91c8bea7fa1fc66343332
egrazm <eliasgonram@gmail.com>
2025-10-07 14:58:20 -0300

    refactor: mejora los comentarios

--- dato_palabras.py
+++ dato_palabras.py
@@ -1,8 +1,10 @@
-#importa libreria random para aleatorizar
+# Importar dependencia para funciones de aleatoriedad
 import random 
 
-#funcion que crea una lista de palabras y asegura las palabras en minusculas retornandolas
 def obtener_lista_palabras():
+    """
+    Funcion que genera una lista de 20 palabras, asegura que esten en minuscula con .lower() y retorna la lista para utilizarla luego.
+    """
     lista = [
     "barro", "juego", "zumba", "calor", "hielo", "lanza", "borde", "salsa", "pulso",
     "brisa", "tacho", "fluir", "nubes", "vigor", "joven", "raton",
@@ -11,8 +13,10 @@ def obtener_lista_palabras():
     lista_minus=[palabra.lower() for palabra in lista]
     return lista_minus
 
-# funcion que selecciona una de las palabras de la lista (usa random libreria importada)
 def seleccionar_palabra_secreta():
+    """
+    Funcion que selecciona una palabra de la lista de manera aleatoria.
+    """
     palabra= obtener_lista_palabras()
     return random.choice(palabra)
 
