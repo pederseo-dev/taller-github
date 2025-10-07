b39bd4b619ffc8102928c9b9fbfaadb28bbce588
Nicolas <nicofleitaspy@gmail.com>
2025-10-07 15:05:48 -0300

    Fix loop infinito :(

--- manejo_entrada.py
+++ manejo_entrada.py
@@ -1,12 +1,13 @@
 def obtener_intento_jugador(numero_intento, intentos_maximos):
         cantidad_intentos = 0
         while cantidad_intentos < intentos_maximos:
+        
             print(f"Intento {cantidad_intentos + 1}/{intentos_maximos}")
             palabra_valida = input("Ingresa una palabra: ").strip().lower()
             if validar_intento(palabra_valida):
                   return palabra_valida
             else:
                 print("Error")
-        cantidad_intentos += 1
+            cantidad_intentos += 1
 
 
